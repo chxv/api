eb873503466cee73287121d9ee767b6fce36c3f7
xander@ubuntu16.04 <26496749+cshsonice@users.noreply.github.com>
2019-03-23 15:56:57 +0800

    add redis

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import os
+from .redis_operation import MyRedis
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY', default='a difficult key for flask')
-    DATABASE_URI = ''
+    ALLOWED_ORIGIN = ('xchens.cn', '127.0.0.1')
 
 
 class DevelopmentConfig(BaseConfig):
-    pass
+    REDIS = MyRedis()
 
 
 class ProductionConfig(BaseConfig):
