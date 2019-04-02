40b4a73a1312bd7780342d34275c3e8d0284ddc4
xander@ubuntu16.04 <26496749+cshsonice@users.noreply.github.com>
2019-04-02 12:08:51 +0800

    add config

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY', default='a difficult key for flask')
     ALLOWED_ORIGIN = ('xchens.cn', '127.0.0.1')
+    MAX_DATA_EXPIRE_TIME = 60*60*24*3  # 3 days
+    PASTE_PUBLIC_KEY_PREFIX = b'PASTE_PUBLIC_'
+    PASTE_SECRET_KEY_PREFIX = b'PASTE_SECRET_'
 
 
 class DevelopmentConfig(BaseConfig):
