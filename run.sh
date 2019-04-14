1547869959c3a97bd637d13e206950a792a5d832
xander@ubuntu16.04 <26496749+cshsonice@users.noreply.github.com>
2019-04-14 14:57:29 +0800

    update run

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+target_dir=$(dirname $0)
+cd $target_dir
+echo "working in ${PWD}"
+
 source src/.env
 gunicorn src.app:app \
--w 4 \
--b 0.0.0.0:80 \
---error-logfile log/flask-error.log \
---access-logfile log/flask-access.log \
---log-level debug \
---limit-request-field_size 8190
+    -w 4 \
+    -b 0.0.0.0:80 \
+    --error-logfile log/flask-error.log \
+    --access-logfile log/flask-access.log \
+    --log-level debug \
+    --limit-request-field_size 8190
+echo "Gunicorn Running..."
+
+
+
