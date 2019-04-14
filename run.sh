8a3cf2355fb79ad484bd2aa4a4005848eb0c612b
xander@ubuntu16.04 <26496749+cshsonice@users.noreply.github.com>
2019-04-14 10:54:05 +0800

    add start shell script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+gunicorn src.app:app \
+-w 4 \
+-b 0.0.0.0:80 \
+--error-logfile log/flask-error.log \
+--access-logfile log/flask-access.log \
+--log-level debug \
+--limit-request-field_size 8190
