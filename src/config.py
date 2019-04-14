6a3bfff3d2f97f31a508d59985aedb670386555d
xander@ubuntu16.04 <26496749+cshsonice@users.noreply.github.com>
2019-04-14 16:19:53 +0800

    modify way of read enable_secret_log

--- src/config.py
+++ src/config.py
@@ -3,6 +3,9 @@ from .redis_operation import MyRedis
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+enable_customized_log = os.environ.get('ENABLE_CUSTOMIZED_LOG', default=True)
+enable_customized_log = False if enable_customized_log == '0' else True
+
 
 class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY', default='a difficult key for flask')
@@ -10,7 +13,7 @@ class BaseConfig:
     MAX_DATA_EXPIRE_TIME = 60*60*24*3  # 3 days
     PASTE_PUBLIC_KEY_PREFIX = b'PASTE_PUBLIC_'
     PASTE_SECRET_KEY_PREFIX = b'PASTE_SECRET_'
-    ENABLE_CUSTOMIZED_LOG = os.environ.get('ENABLE_CUSTOMIZED_LOG', default=True)  # my log
+    ENABLE_CUSTOMIZED_LOG = enable_customized_log  # my log
     JSON_AS_ASCII = False  # jsonify response support chinese
 
 
