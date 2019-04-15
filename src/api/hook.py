1fe54757963c7f90a7ca02ad150d8f4f9465fb5e
xander@ubuntu16.04 <26496749+cshsonice@users.noreply.github.com>
2019-04-15 19:43:17 +0800

    add cors control

--- src/api/hook.py
+++ src/api/hook.py
@@ -11,8 +11,15 @@ def before_first_req():
 
 @mod.before_app_request
 def before_req():
-    redis = current_app.config['REDIS']
-    g.redis = redis.connect()
+    ip, protocol, referer = get_remote_info()
+    host, port = analysis_referer(referer)
+    if host in current_app.config.get("ALLOWED_ORIGIN", []):
+        g.CORS_valid = True
+        # connect database when valid CORS
+        redis = current_app.config['REDIS']
+        g.redis = redis.connect()
+    else:
+        g.CORS_valid = False
 
 
 @mod.after_app_request
@@ -21,9 +28,15 @@ def after_req(response):
     in case of an unhandled exception occurred.
     """
     try:
+        ip, protocol, referer = get_remote_info()
+        host, port = analysis_referer(referer)
+        if host in current_app.config.get("ALLOWED_ORIGIN", []):
+            # CORS
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Methods'] = 'POST,GET,OPTIONS'
+            response.headers['Access-Control-Allow-Headers'] = 'X-PINGOTHER,Content-Type'
         # log when need
         if current_app.config.get('ENABLE_CUSTOMIZED_LOG', False):
-            ip, protocol = get_remote_info()
             http_method = request.method
             path = request.environ.get('REQUEST_URI', request.path)
             code = response.status_code
@@ -36,8 +49,38 @@ def after_req(response):
 
 def get_remote_info():
     """ip protocol"""
-    return request.remote_addr, request.environ.get('SERVER_PROTOCOL')
+    return request.remote_addr, \
+        request.environ.get('SERVER_PROTOCOL'), \
+        request.referrer
 
 
+def analysis_referer(referer: str):
+    try:
+        # 简单校验
+        if not referer or len(referer) < 3:
+            return '', 0
+        # 去除协议头
+        referer = referer.strip('/')
+        if referer.startswith('https://'):
+            referer = referer[8:]
+        elif referer.startswith('http://'):
+            referer = referer[7:]
+        else:
+            return '', 0  # host, port
+        # 去除路径
+        if '/' in referer:
+            t = referer.split('/', 1)
+            referer = t[0]
+        # 获得host, port
+        if ':' in referer:
+            r = referer.split(':')
+            if len(r) == 2 and r[1].isdigit():
+                return r[0], int(r[1])
+            else:
+                return '', 0
+        return referer, 80
 
+    except Exception as e:
+        print('analysis referer error: ', repr(e))
+        return '', 0
 
