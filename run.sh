678ab49a5b15bb768baf3e82f5710e692ae11c01
xander@ubuntu16.04 <26496749+cshsonice@users.noreply.github.com>
2019-04-17 16:39:11 +0800

    update

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 target_dir=$(dirname $0)
-cd $target_dir
+cd ${target_dir}
 echo "working in ${PWD}"
 
 while read line; do eval "export $line && echo $line"; done < .env
 echo -e "Gunicorn Running... \n"
 gunicorn src.app:app \
     -w 4 \
-    -b 0.0.0.0:80 \
+    -b 0.0.0.0:${PORT} \
     --error-logfile log/flask-error.log \
     --access-logfile log/flask-access.log \
     --log-level debug \
