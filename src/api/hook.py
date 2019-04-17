178b32a23926c82991a43546325f7a6d9e2dd018
xander@ubuntu16.04 <26496749+cshsonice@users.noreply.github.com>
2019-04-17 16:06:29 +0800

    fix bug and adjust for nginx

--- src/api/hook.py
+++ src/api/hook.py
@@ -11,13 +11,13 @@ def before_first_req():
 
 @mod.before_app_request
 def before_req():
-    ip, protocol, referer = get_remote_info()
-    host, port = analysis_referer(referer)
-    if host in current_app.config.get("ALLOWED_ORIGIN", []):
+    ip, host, scheme = get_remote_info()
+    # connect database
+    redis = current_app.config['REDIS']
+    g.redis = redis.connect()
+    # confirm cors to set response's headers
+    if host in current_app.config.get("ALLOWED_HOST", []):
         g.CORS_valid = True
-        # connect database when valid CORS
-        redis = current_app.config['REDIS']
-        g.redis = redis.connect()
     else:
         g.CORS_valid = False
 
@@ -28,19 +28,19 @@ def after_req(response):
     in case of an unhandled exception occurred.
     """
     try:
-        ip, protocol, referer = get_remote_info()
-        host, port = analysis_referer(referer)
-        if host in current_app.config.get("ALLOWED_ORIGIN", []):
+        ip, host, scheme = get_remote_info()
+        if g.CORS_valid:
             # CORS
-            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Origin'] = '{}://{}'.format(scheme, host)
             response.headers['Access-Control-Allow-Methods'] = 'POST,GET,OPTIONS'
             response.headers['Access-Control-Allow-Headers'] = 'X-PINGOTHER,Content-Type'
+            response.headers['Access-Control-Max-Age'] = '86400'
         # log when need
         if current_app.config.get('ENABLE_CUSTOMIZED_LOG', False):
             http_method = request.method
             path = request.environ.get('REQUEST_URI', request.path)
             code = response.status_code
-            log_info = "{} [{} {}] {} {}".format(ip, http_method, path, protocol, code)
+            log_info = "{} [{} {}] {}".format(ip, http_method, path, code)
             # current_app.logger.info(log_info)
             log(log_info, target='my-request.log', isprint=True)
     finally:
@@ -48,10 +48,14 @@ def after_req(response):
 
 
 def get_remote_info():
-    """ip protocol"""
-    return request.remote_addr, \
-        request.environ.get('SERVER_PROTOCOL'), \
-        request.referrer
+    """
+    :return ip, host, scheme
+    """
+    if current_app.config.get("REVERSE_PROXY", "").lower() == 'nginx':
+        return str(request.headers.get('X-Real-IP')), \
+               str(request.headers.get('Host')), \
+               str(request.headers.get('X-Real-Scheme'))
+    return request.remote_addr, request.host, request.scheme
 
 
 def analysis_referer(referer: str):
@@ -65,8 +69,6 @@ def analysis_referer(referer: str):
             referer = referer[8:]
         elif referer.startswith('http://'):
             referer = referer[7:]
-        else:
-            return '', 0  # host, port
         # 去除路径
         if '/' in referer:
             t = referer.split('/', 1)
